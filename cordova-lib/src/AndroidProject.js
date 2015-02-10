08464158ebaebeecf115f1afadb83a2cf0575a6b
Mark Koudritsky <kamrik@gmail.com>
2015-02-10 14:54:28 -0500

    Split to CordovaProject and AndroidProject

---
+++ cordova-lib/src/AndroidProject.js
@@ -0,0 +1,11 @@
+var PlatformProject = require('PlatformProject');
+function AndroidProject(root) {
+
+}
+
+/*
+AndroidProject.prototype.funcName = funcName;
+function funcName(plugins, opts) {
+
+}
+*/
